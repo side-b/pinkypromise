83c125b04f99f78e1c001f533b159b5c5e2f41bb
Pierre Bertet <hi@bpier.re>
2022-12-15 17:38:15 +0000

    app wip

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,12 @@
-import {
-  connectorsForWallets,
-  RainbowKitProvider,
-} from "@rainbow-me/rainbowkit";
-import {
-  injectedWallet,
-  metaMaskWallet,
-  walletConnectWallet,
-} from "@rainbow-me/rainbowkit/wallets";
+import { connectorsForWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { injectedWallet, metaMaskWallet, walletConnectWallet } from "@rainbow-me/rainbowkit/wallets";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { match } from "ts-pattern";
 import { configureChains, createClient, WagmiConfig } from "wagmi";
 import { hardhat, mainnet, polygon, sepolia } from "wagmi/chains";
 import { infuraProvider } from "wagmi/providers/infura";
+import { jsonRpcProvider } from "wagmi/providers/jsonRpc";
 import { publicProvider } from "wagmi/providers/public";
 import { App } from "./App";
 import { INFURA_KEY, NETWORKS } from "./environment";
@@ -25,12 +19,19 @@ const { chains, provider, webSocketProvider } = configureChains(
       .with("mainnet", () => mainnet)
       .with("polygon", () => polygon)
       .with("sepolia", () => sepolia)
-      .with("localhost", () => hardhat)
+      .with("local", () => hardhat)
       .otherwise(() => "")
   ).filter(Boolean),
   [
-    infuraProvider({ apiKey: INFURA_KEY }),
-    publicProvider(),
+    infuraProvider({ priority: 1, apiKey: INFURA_KEY }),
+    publicProvider({ priority: 2 }),
+    jsonRpcProvider({
+      rpc: (chain) => (
+        chain.id === hardhat.id
+          ? ({ http: "http://localhost:8545" })
+          : null
+      ),
+    }),
   ],
 );
 
--- src/types.ts
+++ src/types.ts
@@ -2,13 +2,13 @@ export type Address = `0x${string}`;
 
 // from ethers https://github.com/ethers-io/ethers.js/blob/c80fcddf50a9023486e9f9acb1848aba4c19f7b6/packages/abstract-provider/lib.esm/index.d.ts#L54-L64
 export interface Log {
-    blockNumber: number;
-    blockHash: string;
-    transactionIndex: number;
-    removed: boolean;
-    address: string;
-    data: string;
-    topics: Array<string>;
-    transactionHash: string;
-    logIndex: number;
+  blockNumber: number;
+  blockHash: string;
+  transactionIndex: number;
+  removed: boolean;
+  address: string;
+  data: string;
+  topics: Array<string>;
+  transactionHash: string;
+  logIndex: number;
 }
