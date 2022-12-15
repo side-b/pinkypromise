dff1132238d343070fde2d5fca647658adf454b7
Pierre Bertet <hi@bpier.re>
2022-12-15 17:43:12 +0000

    Add contracts/deploy.sh

--- contracts/deploy-sepolia.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-source ./.env
-
-forge script script/PinkySwearPacts.s.sol:PinkySwearPactsScript --rpc-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv
---
+++ contracts/deploy.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+source ./.env
+
+RPC_URL=""
+VERIFY=""
+CHAIN_ID=
+
+if [[ "$1" == "local" ]]; then
+    RPC_URL=$RPC_URL_LOCAL
+    CHAIN_ID="31337"
+
+elif [[ "$1" == "sepolia" ]]; then
+    RPC_URL=$RPC_URL_SEPOLIA
+    VERIFY="--verify"
+    CHAIN_ID="11155111"
+else
+    echo "Must provide a network to deploy to:" 1>&2
+    echo "  " 1>&2
+    echo "  ./deploy.sh <local | sepolia>" 1>&2
+    echo "  " 1>&2
+    exit 1
+fi
+
+forge script script/PinkySwearPacts.s.sol:PinkySwearPactsScript --rpc-url $RPC_URL --broadcast $VERIFY
+
+CONTRACT_ADDRESS=$(cat /home/pierre/d/pinkyswearpacts/contracts/broadcast/PinkySwearPacts.s.sol/$CHAIN_ID/run-latest.json | jq '.transactions[] | select(.contractName == "PinkySwearPacts" and .transactionType == "CREATE") .contractAddress' --raw-output)
+
+echo ""
+echo "    Contract address: ${CONTRACT_ADDRESS}"
+echo ""
+echo ""
