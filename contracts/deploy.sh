13dd8b033cb2fc57dd9ca206466ec6e1ece6c355
Pierre Bertet <hi@bpier.re>
2022-12-19 19:10:52 +0000

    deploy.sh: path fix

--- contracts/deploy.sh
+++ contracts/deploy.sh
@@ -15,7 +15,7 @@ elif [[ "$1" == "sepolia" ]]; then
     VERIFY="--verify"
     CHAIN_ID="11155111"
 else
-    echo "Must provide a network to deploy to:" 1>&2
+    echo "Must provide a network:" 1>&2
     echo "  " 1>&2
     echo "  ./deploy.sh <local | sepolia>" 1>&2
     echo "  " 1>&2
@@ -24,7 +24,7 @@ fi
 
 forge script script/PinkySwearPacts.s.sol:PinkySwearPactsScript --rpc-url $RPC_URL --broadcast $VERIFY
 
-CONTRACT_ADDRESS=$(cat /home/pierre/d/pinkyswearpacts/contracts/broadcast/PinkySwearPacts.s.sol/$CHAIN_ID/run-latest.json | jq '.transactions[] | select(.contractName == "PinkySwearPacts" and .transactionType == "CREATE") .contractAddress' --raw-output)
+CONTRACT_ADDRESS=$(cat broadcast/PinkySwearPacts.s.sol/$CHAIN_ID/run-latest.json | jq '.transactions[] | select(.contractName == "PinkySwearPacts" and .transactionType == "CREATE") .contractAddress' --raw-output)
 
 echo ""
 echo "    Contract address: ${CONTRACT_ADDRESS}"
--- contracts/script/PinkySwearPacts.s.sol
+++ contracts/script/PinkySwearPacts.s.sol
@@ -5,14 +5,9 @@ import "forge-std/Script.sol";
 import "../src/PinkySwearPacts.sol";
 
 contract PinkySwearPactsScript is Script {
-    function setUp() public {}
-
     function run() external {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-        vm.startBroadcast(deployerPrivateKey);
-
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         new PinkySwearPacts("Pinky Swear Pacts", "PSP");
-
         vm.stopBroadcast();
     }
 }
