0ad931d3558ecb8c484c9bd12dd45d1ce4099735
Enrique Ortiz <hi@enriqueortiz.dev>
2023-03-01 15:14:33 -0400

    chore: pin sol version

--- contracts/PinkyPromiseSvg.tpl.sol
+++ contracts/PinkyPromiseSvg.tpl.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity 0.8.17;
 
 import "solmate/utils/LibString.sol";
 import {StrSlice, toSlice} from "dk1a-stringutils/StrSlice.sol";
