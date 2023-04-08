39485ecdd18c02ef39652f060cb1cd832a6c1e94
Pierre Bertet <hi@bpier.re>
2023-04-08 15:12:08 +0100

    Add pnpm export

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 
+const EXPORT = process.env.EXPORT === "true";
+
 module.exports = async () => {
   /** @type {import('next').NextConfig} */
   const nextConfig = {
     reactStrictMode: true,
+    output: EXPORT ? "export" : undefined,
+    images: EXPORT ? { unoptimized: true } : undefined,
   };
 
   return withBundleAnalyzer(nextConfig);
