83b451d81f29d1971a603d739ccb6ead7685cfe6
Pierre Bertet <hi@bpier.re>
2023-04-21 15:56:53 +0100

    Add Vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Analytics } from "@vercel/analytics/react";
 import Head from "next/head";
 import { Breakpoint } from "../components/Breakpoint";
 import { Ethereum } from "../components/Ethereum";
@@ -80,6 +81,7 @@ export default function App({ Component, pageProps }: AppProps) {
           </Breakpoint>
         </Ethereum>
       </QueryClientProvider>
+      <Analytics />
     </>
   );
 }
